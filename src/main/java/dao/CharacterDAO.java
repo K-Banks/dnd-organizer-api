2666188715a58423d95198c4f6a241a3c10da03d
Kayl Eubanks <kayleubanks@gmail.com>
2018-06-15 10:55:43 -0700

    Update DAO interface with required methods

--- src/main/java/dao/CharacterDAO.java
+++ src/main/java/dao/CharacterDAO.java
@@ -1,4 +1,24 @@
 package dao;
 
+import java.util.HashMap;
+import java.util.List;
+
 public interface CharacterDAO {
+
+    //create
+    void add(Character newCharacter);
+
+
+    //read
+    List<Character> getAll();
+    Character findById(int characterId);
+
+
+    //update
+    void update(HashMap<String, String> propertiesToUpdate, int characterUpdatedId);
+
+
+    //delete
+    void deleteById(int characterId);
+
 }
