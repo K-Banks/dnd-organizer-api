5b80aba45a6eae195a0cb31a771fccb3a370754f
Kayl Eubanks <kayleubanks@gmail.com>
2018-06-15 10:35:43 -0700

    Pass tests for all getters and setters for Spell model

--- src/test/java/models/SpellTest.java
+++ src/test/java/models/SpellTest.java
@@ -13,55 +13,94 @@ public class SpellTest {
     }
 
     @Test
-    public void name() {
+    public void testGetterAndSetterForName() {
+        Spell spell = setupSpell();
+        spell.setName("Cantrip");
+        assertNotEquals("test", spell.getName());
     }
 
     @Test
-    public void name() {
+    public void testGetterAndSetterForDescription() {
+        Spell spell = setupSpell();
+        spell.setDescription("Cantrip");
+        assertNotEquals("test", spell.getDescription());
     }
 
     @Test
-    public void name() {
+    public void testGetterAndSetterForSchool() {
+        Spell spell = setupSpell();
+        spell.setSchool("Cantrip");
+        assertNotEquals("test", spell.getSchool());
     }
 
     @Test
-    public void name() {
+    public void testGetterAndSetterForLevel() {
+        Spell spell = setupSpell();
+        spell.setLevel(4);
+        assertNotEquals(3, spell.getLevel());
     }
 
     @Test
-    public void name() {
+    public void testGetterAndSetterForCastTime() {
+        Spell spell = setupSpell();
+        spell.setCastTime("Cantrip");
+        assertNotEquals("test", spell.getCastTime());
     }
 
     @Test
-    public void name() {
+    public void testGetterAndSetterForRitual() {
+        Spell spell = setupSpell();
+        spell.setRitual(false);
+        assertNotEquals("test", spell.isRitual());
     }
 
     @Test
-    public void name() {
+    public void testGetterAndSetterForConcentration() {
+        Spell spell = setupSpell();
+        spell.setConcentration(false);
+        assertNotEquals("test", spell.isConcentration());
     }
 
     @Test
-    public void name() {
+    public void testGetterAndSetterForDuration() {
+        Spell spell = setupSpell();
+        spell.setDuration("Cantrip");
+        assertNotEquals("test", spell.getDuration());
     }
 
     @Test
-    public void name() {
+    public void testGetterAndSetterForRange() {
+        Spell spell = setupSpell();
+        spell.setRange("Cantrip");
+        assertNotEquals("test", spell.getRange());
     }
 
     @Test
-    public void name() {
+    public void testGetterAndSetterForVerbal() {
+        Spell spell = setupSpell();
+        spell.setVerbal(false);
+        assertNotEquals("test", spell.isVerbal());
     }
 
     @Test
-    public void name() {
+    public void testGetterAndSetterForSomatic() {
+        Spell spell = setupSpell();
+        spell.setSomatic(false);
+        assertNotEquals("test", spell.isSomatic());
     }
 
     @Test
-    public void name() {
+    public void testGetterAndSetterForMaterial() {
+        Spell spell = setupSpell();
+        spell.setMaterial("Cantrip");
+        assertNotEquals("test", spell.getMaterial());
     }
 
     @Test
-    public void name() {
+    public void testGetterAndSetterForId() {
+        Spell spell = setupSpell();
+        spell.setId(3);
+        assertNotEquals(null, spell.getId());
     }
 
     //Helper
