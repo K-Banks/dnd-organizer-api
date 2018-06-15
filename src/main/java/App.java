c9a80d6edd0716668b6d9ae9e627de475c267b5e
Kayl Eubanks <kayleubanks@gmail.com>
2018-06-15 14:48:53 -0700

    Import and instantiate all needed resoures and components

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,29 @@
+import com.google.gson.Gson;
+import dao.Sql2oCharacterDao;
+import dao.Sql2oClassDao;
+import dao.Sql2oSpellDao;
+import models.Class;
+import models.Character;
+import models.Spell;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import static spark.Spark.*;
+
 public class App {
+
+    public static void main(String[] args) {
+        Sql2oSpellDao spellDao;
+        Sql2oClassDao classDao;
+        Sql2oCharacterDao characterDao;
+        Connection con;
+        Gson gson = new Gson();
+
+        String connectionString = "jdbc:h2:`/jadle.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+
+        spellDao = new Sql2oSpellDao(sql2o);
+        classDao = new Sql2oClassDao(sql2o);
+        characterDao = new Sql2oCharacterDao(sql2o);
+        con = sql2o.open();
+    }
 }
