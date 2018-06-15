85149eabb613856db2e1b8142f97e58517e9f664
Kayl Eubanks <kayleubanks@gmail.com>
2018-06-15 15:28:40 -0700

    Add comments for each route required for minimum functionality

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -25,5 +25,41 @@ public class App {
         classDao = new Sql2oClassDao(sql2o);
         characterDao = new Sql2oCharacterDao(sql2o);
         con = sql2o.open();
+
+        //GET all spells
+
+        //GET all classes
+
+        //GET all characters
+
+        //GET spell details by id
+
+        //GET character details by id
+
+        //GET class details by id
+
+        //GET all spells for a class
+
+        //POST new character
+
+        //POST new class
+
+        //POST new spell
+
+        //POST assign spell to a class
+
+        //POST assign class to a character
+
+        //UPDATE character by id
+
+        //UPDATE class by id
+
+        //UPDATE spell by id
+
+        //DELETE character by id
+
+        //DELETE class by id
+
+        //DELETE spell by id
     }
 }
