c6ec3b9de03b87c073d514c184c64ffdbd49f90a
Kayl Eubanks <kayleubanks@gmail.com>
2018-06-15 09:59:43 -0700

    Create database tables

---
+++ src/main/resources/db/create.sql
@@ -0,0 +1,37 @@
+SET MODE PostgreSQL;
+
+CREATE TABLE IF NOT EXISTS characters (
+    id int PRIMARY KEY auto_increment,
+    name VARCHAR,
+    class_id INTEGER,
+    race VARCHAR,
+    level INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS classes (
+    id int PRIMARY KEY auto_increment,
+    name VARCHAR,
+    description VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS spells(
+    id int PRIMARY KEY auto_increment,
+    name VARCHAR,
+    description VARCHAR,
+    school VARCHAR,
+    level INTEGER,
+    castTime VARCHAR,
+    ritual BOOLEAN,
+    concentration BOOLEAN,
+    duration VARCHAR,
+    range VARCHAR,
+    verbal BOOLEAN,
+    somatic BOOLEAN,
+    material VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS classes_spells (
+    id int PRIMARY KEY auto_increment,
+    class_id INTEGER,
+    spell_id INTEGER
+);
