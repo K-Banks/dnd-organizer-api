bf4829e4289cd0ed91db80f8e36b96d71a8f00b6
Kayl Eubanks <kayleubanks@gmail.com>
2018-06-15 13:35:38 -0700

    Pass test for update method

--- src/main/java/dao/Sql2oClassDao.java
+++ src/main/java/dao/Sql2oClassDao.java
@@ -5,6 +5,7 @@ import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 import org.sql2o.Sql2oException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -51,7 +52,25 @@ public class Sql2oClassDao implements ClassDAO{
 
     @Override
     public void update(HashMap<String, Object> propertiesToUpdate, int classUpdatedId) {
-
+        int updates = propertiesToUpdate.size();
+        ArrayList<String> properties = new ArrayList<>();
+        ArrayList<Object> values = new ArrayList<>();
+        for (String key: propertiesToUpdate.keySet()) {
+            properties.add(key);
+            values.add(propertiesToUpdate.get(key));
+        }
+        try (Connection con = sql2o.open()) {
+            int i = 0;
+            while (i < updates) {
+                con.createQuery("UPDATE classes SET (" + properties.get(i) + ") = (:" + properties.get(i) + ") WHERE id=:id")
+                        .addParameter(properties.get(i), values.get(i))
+                        .addParameter("id", classUpdatedId)
+                        .executeUpdate();
+                i = i+1;
+            }
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
     }
 
     @Override
