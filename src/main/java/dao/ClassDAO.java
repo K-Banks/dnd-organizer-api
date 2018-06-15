afb68cf6069ab16b5ac3b9dbb259b42a2d7a37ff
Kayl Eubanks <kayleubanks@gmail.com>
2018-06-15 11:47:50 -0700

    Update DAO interface with required methods

--- src/main/java/dao/ClassDAO.java
+++ src/main/java/dao/ClassDAO.java
@@ -1,4 +1,20 @@
 package dao;
 
+import java.util.HashMap;
+import java.util.List;
+import models.Class;
+
 public interface ClassDAO {
+    //create
+    void add(Class newClass);
+
+    //read
+    List<Class> getAll();
+    Class findById(int classId);
+
+    //update
+    void update(HashMap<String, Object> propertiesToUpdate, int classUpdatedId);
+
+    //delete
+    void deleteById(int classId);
 }
--- src/main/java/dao/Sql2oClassDao.java
+++ src/main/java/dao/Sql2oClassDao.java
@@ -1,4 +1,14 @@
 package dao;
 
+import org.sql2o.Sql2o;
+
 public class Sql2oClassDao implements ClassDAO{
+
+    private final Sql2o sql2o;
+
+    public Sql2oClassDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+
 }
