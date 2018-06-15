b248f97430445b84d359e85e2732f247d906e0cb
Kayl Eubanks <kayleubanks@gmail.com>
2018-06-15 13:42:57 -0700

    Add required methods

--- src/main/java/dao/SpellDAO.java
+++ src/main/java/dao/SpellDAO.java
@@ -1,4 +1,21 @@
 package dao;
 
+import java.util.HashMap;
+import java.util.List;
+import models.Spell;
+
 public interface SpellDAO {
+    //create
+    void add(Spell newSpell);
+    void assignSpellToClass(int spellId, int classId);
+
+    //read
+    List<Spell> getAll();
+    Spell findById(int spellId);
+
+    //update
+    void update(HashMap<String, Object> propertiesToUpdate, int spellUpdatedId);
+
+    //delete
+    void deleteById(int spellId);
 }
