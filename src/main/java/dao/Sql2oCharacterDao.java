59ff4b2483d50d6f67d7870f2d1141116cb10dea
Kayl Eubanks <kayleubanks@gmail.com>
2018-06-15 10:56:47 -0700

    Insert Override methods into Dao and create corresponding test file

--- src/main/java/dao/Sql2oCharacterDao.java
+++ src/main/java/dao/Sql2oCharacterDao.java
@@ -1,4 +1,37 @@
 package dao;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class Sql2oCharacterDao implements CharacterDAO {
+
+    public Sql2oCharacterDao() {
+    }
+
+    @Override
+    public void add(Character newCharacter) {
+
+    }
+
+    @Override
+    public List<Character> getAll() {
+        return null;
+    }
+
+    @Override
+    public Character findById(int characterId) {
+        return null;
+    }
+
+    @Override
+    public void update(HashMap<String, String> propertiesToUpdate, int characterUpdatedId) {
+
+    }
+
+    @Override
+    public void deleteById(int characterId) {
+
+    }
+
+
 }
